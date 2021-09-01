2dfdf9db309d0c78e3d0269e942a234a4abdaf3b
rlatron <89917976+rlatron@users.noreply.github.com>
2021-09-01 15:30:08 +0200

    Implement the two filter functions

--- _init_.py
+++ _init_.py
@@ -10,7 +10,7 @@ class entry:
         self.points = points
 
 def fillEntries(entries):
-    urlpage =  'https://news.ycombinator.com/news?p=2'
+    urlpage =  'https://news.ycombinator.com/news'
     page = urllib.request.urlopen(urlpage)
     soup = BeautifulSoup(page, 'html.parser')
     
@@ -18,24 +18,47 @@ def fillEntries(entries):
     table=soup.find('table')
     results = table.find_all('tr')[3].find_all('tr')
     
-    for i in range (0, len(results), 3):
+    for i in range (0, len(results)-2, 3):
     
         indexStart = results[i].text.find('.') + 2
         indexEnd = results[i].text.find('(')-1
         title = results[i].text[indexStart:indexEnd]
-        order = results[i].text[1:indexStart-2]
+        order = int(results[i].text[1:indexStart-2])
         idx = results[i+1].text.find(' ')
-        points = results[i+1].text[1:idx]
+        points = int(results[i+1].text[1:idx])
         indexStart = results[i+1].text.find(' | hide | ') + 10
         indexEnd = results[i+1].text.find(' comments')
-        comments = results[i+1].text[indexStart:indexEnd]
+        #if the comments string wasn't found then there is no comments
+        if indexEnd == -1:
+            comments=0
+        else:
+            comments = int(results[i+1].text[indexStart:indexEnd])
         entries.append(entry(title, order, comments, points))
+
+def moreThanFiveWordsFilter(entries):
+    returnedEntries = []
+    for entry in entries:
+        if len(entry.title.split()) > 5:
+            returnedEntries.append(entry)  
+    returnedEntries.sort(key=lambda x: x.comments)
+    return returnedEntries     
+
+def lessThanFiveWordsFilter(entries):
+    returnedEntries = []
+    for entry in entries:
+        if len(entry.title.split()) <= 5:
+            returnedEntries.append(entry)  
+    returnedEntries.sort(key=lambda x: x.points)
+    return returnedEntries  
+
                 
-entries = []
-fillEntries(entries)
-
-
-print()
+def main():
+    entries = []
+    fillEntries(entries)
+    filteredEntriesFiveMore = moreThanFiveWordsFilter(entries)
+    filteredEntriesFiveLess = lessThanFiveWordsFilter(entries)
+    assert len(filteredEntriesFiveMore) + len(filteredEntriesFiveLess) == len(entries)
     
+if __name__ == "__main__":
+    main()
     
-
