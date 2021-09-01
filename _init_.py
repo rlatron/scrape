52a27b5c059c2322537ee7157372599d0c4fabb7
rlatron <89917976+rlatron@users.noreply.github.com>
2021-09-01 14:39:20 +0200

    Scrape 30 entries from the website and store them into an entry array

--- _init_.py
+++ _init_.py
@@ -1,1 +1,41 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+
+class entry:
+    def __init__(self, title, order, comments, points):
+        self.title = title
+        self.order = order
+        self.comments = comments
+        self.points = points
+
+def fillEntries(entries):
+    urlpage =  'https://news.ycombinator.com/news?p=2'
+    page = urllib.request.urlopen(urlpage)
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    
+    table=soup.find('table')
+    results = table.find_all('tr')[3].find_all('tr')
+    
+    for i in range (0, len(results), 3):
+    
+        indexStart = results[i].text.find('.') + 2
+        indexEnd = results[i].text.find('(')-1
+        title = results[i].text[indexStart:indexEnd]
+        order = results[i].text[1:indexStart-2]
+        idx = results[i+1].text.find(' ')
+        points = results[i+1].text[1:idx]
+        indexStart = results[i+1].text.find(' | hide | ') + 10
+        indexEnd = results[i+1].text.find(' comments')
+        comments = results[i+1].text[indexStart:indexEnd]
+        entries.append(entry(title, order, comments, points))
+                
+entries = []
+fillEntries(entries)
+
+
+print()
+    
+    
 
