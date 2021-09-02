bfc60f68e7d7d45a711b1abc6038438d829c1488
rlatron <89917976+rlatron@users.noreply.github.com>
2021-09-02 18:22:59 +0200

    Add tests

--- _init_.py
+++ _init_.py
@@ -9,23 +9,23 @@ class entry:
         self.comments = comments
         self.points = points
 
-def fillEntries(entries):
-    urlpage =  'https://news.ycombinator.com/news'
-    page = urllib.request.urlopen(urlpage)
-    soup = BeautifulSoup(page, 'html.parser')
+def fill30Entries(entries, url):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser') 
     
-    
-    table=soup.find('table')
+    table = soup.find('table')
     results = table.find_all('tr')[3].find_all('tr')
     
     for i in range (0, len(results)-2, 3):
-    
+        #find the title and the order
         indexStart = results[i].text.find('.') + 2
         indexEnd = results[i].text.find('(')-1
         title = results[i].text[indexStart:indexEnd]
         order = int(results[i].text[1:indexStart-2])
-        idx = results[i+1].text.find(' ')
-        points = int(results[i+1].text[1:idx])
+        #find the number of points
+        index = results[i+1].text.find(' ')
+        points = int(results[i+1].text[1:index])
+        #find the number of comments
         indexStart = results[i+1].text.find(' | hide | ') + 10
         indexEnd = results[i+1].text.find(' comments')
         #if the comments string wasn't found then there is no comments
@@ -51,13 +51,33 @@ def lessThanFiveWordsFilter(entries):
     returnedEntries.sort(key=lambda x: x.points)
     return returnedEntries  
 
+def tests(entries, filteredEntriesMoreFive, filteredEntriesLessFive):
+    #test that we only took only 30 entries and the consistency between both filters
+    assert len(entries) == 30
+    assert len(filteredEntriesMoreFive) + len(filteredEntriesLessFive) == len(entries)
+    #check that the entries are ordered by number of comments and that the titles have more than five words
+    for i in range(1, len(filteredEntriesMoreFive)):
+        assert filteredEntriesMoreFive[i].comments >= filteredEntriesMoreFive[i-1].comments 
+        assert len(filteredEntriesMoreFive[i].title.split()) > 5 
+    #check that the entries are ordered by number of points and that the titles have less or equal to five words
+    for i in range(1, len(filteredEntriesLessFive)):
+        assert filteredEntriesLessFive[i].points >= filteredEntriesLessFive[i-1].points 
+        assert len(filteredEntriesLessFive[i].title.split()) <= 5
+    
                 
 def main():
     entries = []
-    fillEntries(entries)
-    filteredEntriesFiveMore = moreThanFiveWordsFilter(entries)
-    filteredEntriesFiveLess = lessThanFiveWordsFilter(entries)
-    assert len(filteredEntriesFiveMore) + len(filteredEntriesFiveLess) == len(entries)
+    fill30Entries(entries, 'https://news.ycombinator.com/news')
+    filteredEntriesMoreFive = moreThanFiveWordsFilter(entries)
+    filteredEntriesLessFive = lessThanFiveWordsFilter(entries)
+    tests(entries, filteredEntriesMoreFive, filteredEntriesLessFive)
+    
+    #try again with page 2
+    entries = []
+    fill30Entries(entries, 'https://news.ycombinator.com/news?p=2')
+    filteredEntriesMoreFive = moreThanFiveWordsFilter(entries)
+    filteredEntriesLessFive = lessThanFiveWordsFilter(entries)
+    tests(entries, filteredEntriesMoreFive, filteredEntriesLessFive)
     
 if __name__ == "__main__":
     main()
